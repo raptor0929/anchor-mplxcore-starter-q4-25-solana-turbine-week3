a9c0eebbbc843e059d5450cefb60e8b21a1164f1
raptor0929 <flaura.dev@gmail.com>
2026-02-08 18:51:02 -0300

    topic: mpl_core
    - freeze nft
    - thaw nft
    - update nft

--- programs/anchor-mplxcore-q4-25/src/lib.rs
+++ programs/anchor-mplxcore-q4-25/src/lib.rs
@@ -7,7 +7,7 @@ mod error;
 use instructions::*;
 // use state::*;
 
-declare_id!("3feeK7HhTHmmr9ofcsNSb5QjQ6c8Vyhsko6E377Eo2yC");
+declare_id!("FUayXQ8jv3thHBewkgpYACzozrGaUamabj96h9wTJco1");
 
 #[program]
 pub mod anchor_mplxcore_q4_25 {
@@ -25,15 +25,15 @@ pub mod anchor_mplxcore_q4_25 {
         ctx.accounts.mint_nft()
     }
 
-    // pub fn freeze_nft(ctx: Context<FreezeNft>) -> Result<()> {
-    //     ctx.accounts.freeze_nft()
-    // }
+    pub fn freeze_nft(ctx: Context<FreezeNft>) -> Result<()> {
+        ctx.accounts.freeze_nft()
+    }
 
-    // pub fn thaw_nft(ctx: Context<ThawNft>) -> Result<()> {
-    //     ctx.accounts.thaw_nft()
-    // }
+    pub fn thaw_nft(ctx: Context<ThawNft>) -> Result<()> {
+        ctx.accounts.thaw_nft()
+    }
 
-    // pub fn thaw_nft(ctx: Context<UpdateNft>, new_name: String) -> Result<()> {
-    //     ctx.accounts.update_nft(new_name)
-    // }
+    pub fn update_nft(ctx: Context<UpdateNft>, new_name: String) -> Result<()> {
+        ctx.accounts.update_nft(new_name)
+    }
 }
